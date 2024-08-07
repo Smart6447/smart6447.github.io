ab948a1775aa4e6b68a193c89f7bd3dbed702c22
Smart6447 <smart.nakamol@gmail.com>
2024-08-07 23:04:59 +0700

    Adjust font.js

--- script.js
+++ script.js
@@ -105,19 +105,19 @@ function renderSecondPage() {
        
             <h1 class="text-xl font-bold text-center mb-3">Evaluation criteria</h1><br>
 
-            <p class="text-lg text-center mb-4 text-[#ff668f]">
+            <p class="text-xl text-center mb-4 text-[#ff668f]">
             The service quality of staff and doctor<br>
             เกณฑ์คุณภาพการให้บริการของพนักงาน/หมอ<br><br>
             </p>
-            <p class="text-lg text-center mb-4 text-[#ff668f]">
-            <ul style="list-style-type: none; padding-left: 5em;">
+            <p class="text-xl text-center mb-3 text-[#ff668f]">
+            <ul style="list-style-type: none; padding-left: 4em;">
             <li style="color: #ff668f;">"5 stars" Perfect - ดีเยี่ยม</li>
             <li style="color: #ff668f;">"4 stars" Over expectation - ดี</li>
             <li style="color: #ff668f;">"3 stars" Satisfy - พอใจ</li>
             <li style="color: #ff668f;">"2 stars" Poor - แย่</li>
             <li style="color: #ff668f;">"1 star"  Worst - แย่มาก</li><br>
             </ul>
-            </p><br><br><br>
+            </p><br><br><br><br>
             <p class="text-md text-center mb-4 text-[#ff668f]">
             To give feedback, please write on the last page.<br>
             สำหรับเขียนข้อแนะนำเพิ่มเติมจะอยู่หน้าสุดท้ายนะคะ
@@ -404,7 +404,7 @@ function renderQuestionPage4() {
                 <div class="space-y-2 mb-4  w-full">
                     <textarea id="feedbackInput" class="w-full h-32 p-4 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-pink-600 focus:border-transparent"></textarea>
                 </div>
-                <br><br><br><br><br>
+                <br><br><br><br><br><br><br><br><br>
                 <div class="space-y-2 w-full mb-4 text-center">
                 <button onclick="nextQuestion()" class="w-full max-w-md bg-[#0f513a] text-white py-2 rounded-full text-xl hover:bg-pink-600">
                 ${currentQuestion === questions - 1 ? 'Submit' : 'Next'}
@@ -443,13 +443,16 @@ function renderQuestionPage4() {
 
 function renderThanksPage() {
   app.innerHTML = `
-        <div class="flex flex-col items-center justify-center h-full p-8">
-            <img src="logo.png" alt="Logo" class="mb-6 w-20">
+      
+        <div class="flex flex-col h-full p-8 text-center ">
+            <img src="logo.png" alt="Logo" class="mb-4 w-20 self-center">
+            <br><br><br>
             <h1 class="text-xl font-bold text-center mb-3">" Thank you for being part
             of our development."</h1>
-            
+            <br>
             <p class="text-xl text-center mb-6 text-[#ff668f]">We will fulfill our promise to continually provide
             better quality service.</p>
+            <br><br><br><br>
 
             <button onclick="goToStart(reset=true)" class="w-full max-w-md bg-[#0f513a] text-white py-2 rounded-full text-xl hover:bg-pink-600">Back to home</button>
         </div>
