f0624f6242c60b1c8536a13c388d2620176d786f
Smart6447 <smart.nakamol@gmail.com>
2024-09-25 15:16:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -295,7 +295,7 @@ function renderQuestionPage3() {
   const options = [
     'Dr.Bow หมอโบว์',
     'Dr.Kay หมอเคย์',
-    'Dr.Wan หมอป๊อปปี้',
+    'Dr.Poppy หมอป๊อปปี้',
     'Dr.Ter หมอเต้อ',
     'Other อื่นๆ',
   ];
