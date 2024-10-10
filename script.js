60d827f2cfae1aac010e01e07459511f212692ba
Smart6447 <smart.nakamol@gmail.com>
2024-10-10 15:02:18 +0700

    Update script.js
    Update staffs

--- script.js
+++ script.js
@@ -186,6 +186,8 @@ function renderQuestionPage2() {
     'K.Jane คุณเจน',
     'K.Pim คุณพิม',
     'K.Namwaan คุณน้ำหวาน',
+    'K.Tangmo ตุณแตงโม',
+    'K.A-Ngoon คุณองุ่น',
     'Other อื่นๆ',
   ];
 
